3ebf4f110cfa9207274430ea5955ad46a1ee1696
Guy-pixel <guygasson@hotmail.co.uk>
2023-04-11 10:43:30 +0100

    Swapping class for className in Signup form

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -32,7 +32,7 @@ export default function Signup() {
             })
     }
     return(
-        <div class="login-signup-form animated fadeInDown">
+        <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">
